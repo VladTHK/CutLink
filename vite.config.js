b73ddd231b8be6a84e5302e13e11e046cb38f945
Vladislav <starinkov2020@mail.ru>
2026-06-26 22:58:40 +0300

    fix src

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({mode}) =>{
-  const isProduction = mode === 'production';
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
 
   return {
-    base: isProduction ? '/CutLink/' : '/',
+    base: isProduction ? './' : '/',
     plugins: [react()],
   }
 })
